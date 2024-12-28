dae1bfac3b17258d283872c0a7290fceae5297f6
Ali Tlisov <a.tlisov@1440.space>
2024-12-28 19:59:40 +0300

    removed Models added multimeasurement combination

--- src/Kalman.jl
+++ src/Kalman.jl
@@ -4,9 +4,8 @@ using LinearAlgebra
 using Models
 
 export KalmanEstimator, LinearKalman, KalmanEstimated
-# export modelproc, modelmeas
 export measurement
-export process_update, measurement_update
+export process_update, measurement_update, measurements_update
     
 abstract type KalmanEstimator <: AbstractEstimator end
 abstract type LinearKalman <: KalmanEstimator end
@@ -20,10 +19,34 @@ function KalmanEstimated(v::AbstractVector{T}) where T
     KalmanEstimated(T.(I(length(v)) * 1e6), v)
 end
 
-# modelproc(::AbstractEstimator)::Model = missing
-# modelmeas(::AbstractEstimator)::Model = missing
-# measurement(::Model, y) = Vector(y)
 measurement(::Model, y) = y
+
+function measurements_update(
+    ms::Vector{Model{T}};
+    x::KalmanEstimated{T},
+    ys,
+    u::AbstractVector{T}=T[0]) where T
+
+    P = x.P
+    x = x.x
+    V = typeof(x)
+    
+    H = reduce(vcat, A(m; x, u, y) for (m, y) in zip(ms, ys))
+    K = P * reduce(hcat, A(m; x, u, y)' * pinv(m(P; x, u, y)) for (m, y) in zip(ms, ys))
+
+    P -= K * H * P
+
+    δxs = map(zip(ms, ys)) do (m, y)
+        δy = measurement(m, y) - m(;x, u, y)
+        s = m(P; x, u, y)
+        h = A(m; x, u, y)
+        k = P * h' * pinv(s)
+        δx = k * δy
+    end
+    
+    x = x + sum(δxs) 
+    KalmanEstimated((P + P') / 2, V(x))
+end
 
 function measurement_update(m::Model{T};
                             x::KalmanEstimated{T},
@@ -34,8 +57,8 @@ function measurement_update(m::Model{T};
     V = typeof(x)
     
     S = m(P; x, u, y)
-    C = A(m; x, u, y) # measurement model's A is actually C
-    K = P * C' * pinv(S)
+    H = A(m; x, u, y)
+    K = P * H' * pinv(S)
     P -= K * S * K'
     δy = measurement(m, y) - m(;x, u, y)
     δx = K * δy
